3278883de1bfad955549d93c9fb3d03eb0a54a87
jonsito <jonsito@52e26522-a1c9-48c4-a7af-28ea70901177>
2011-02-10 09:25:02 +0000

    Add AVDNIe Validation Auth Certificate to tests
    git-svn-id: https://forja.cenatic.es/svn/opendnie@237 52e26522-a1c9-48c4-a7af-28ea70901177

--- tests/sign_text/1-extract_certificates.sh
+++ tests/sign_text/1-extract_certificates.sh
@@ -12,4 +12,4 @@ echo "Extract CA certificate"
 pkcs15-tool -v --verify-pin $PIN --read-certificate $CertCAIntermediaDGP --output cardCACertificate.pem
 
 echo "Generate cacerts.pem CA certstore"
-cat ACRAIZ-SHA1.pem ACDNIE001-SHA1.pem ACDNIE002-SHA1.pem ACDNIE003-SHA1.pem cardCACertificate.pem > cacerts.pem
+cat ACRAIZ-SHA1.pem ACDNIE001-SHA1.pem ACDNIE002-SHA1.pem ACDNIE003-SHA1.pem cardCACertificate.pem AVDNIEFNMTSHA1.pem > cacerts.pem
